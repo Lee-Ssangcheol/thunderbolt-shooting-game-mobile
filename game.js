c92acf71a40ccb46f9a640837ee93504e460f129
Lee-Ssangcheol <yes99@hanmail.net>
2025-07-12 22:50:45 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -7,11 +7,11 @@ const TOP_EFFECT_ZONE = 20;  // 상단 효과 무시 영역 (픽셀)
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 // 모바일 속도 조절 (더 느리게 조정)
-const mobileSpeedMultiplier = isMobile ? 0.95 : 1.0;
+const mobileSpeedMultiplier = isMobile ? 1.0 : 1.0;
 
 // 모바일 프레임 제한 (60fps 대신 55fps로 완화)
-const MOBILE_FPS_LIMIT = isMobile ? 95 : 100;
-const MOBILE_FRAME_INTERVAL = 1000 / MOBILE_FPS_LIMIT;
+const MOBILE_FPS_LIMIT = isMobile ? 100 : 100;
+const MOBILE_FRAME_INTERVAL = 800 / MOBILE_FPS_LIMIT;
 
 // 전체화면 상태 추적 변수
 let isFullscreenActive = false;
