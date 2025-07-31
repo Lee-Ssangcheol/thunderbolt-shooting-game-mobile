a8deaf4a885edad742197bb9debc8d7c3f6702e6
Lee-Ssangcheol <yes99@hanmail.net>
2025-07-31 21:17:47 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -3458,7 +3458,7 @@ function handleBullets() {
             // 확산탄 이동
             bullet.x += Math.sin(bullet.angle) * bullet.speed;
             bullet.y -= Math.cos(bullet.angle) * bullet.speed;
-            ctx.fillStyle = '#87CEEB'; // 스카이블루로 변경
+            ctx.fillStyle = '#00CED1';
             ctx.fillRect(bullet.x - bullet.width/2, bullet.y - bullet.height/2, bullet.width, bullet.height);
         } else {
             // 일반 총알 이동
